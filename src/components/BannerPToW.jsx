b5db78a53b6f11222d2f0ce534bcc940f071b2a2
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-02-21 18:48:08 +0100

    fix problem with z-index

--- src/components/BannerPToW.jsx
+++ src/components/BannerPToW.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BannerPToW = () => {
   return (
-    <div className="bg-banner-p-to-w bg-12.5 h-[63px] opacity-30">
+    <div className="bg-banner-p-to-w bg-12.5 h-[63px] opacity-30 ">
 
     </div>
   )
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,7 +8,7 @@ const Modal = ({ selectedImg, setSelectedImg }) => {
 
   return (
     <div
-      className='h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-black/70'
+      className='h-screen w-full fixed left-0 top-0 z-10 flex justify-center items-center bg-black/70'
       onClick={handleClick}>
       <img
         src={selectedImg}
