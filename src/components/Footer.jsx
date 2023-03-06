4c52679b8bf1e0c3d98e6859460740b44bdf4d6c
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-03-06 22:06:15 +0100

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
     <footer className="bg-purple-800 py-4">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <div className="flex flex-wrap">
+        <div className="flex md:flex-row flex-col">
           <Link
             to="/legal"
             className="text-gray-300 hover:text-white mr-4"
