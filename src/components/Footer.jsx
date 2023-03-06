1361ea85ebfdeeaba587149d71e4e1fd390afdc1
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-03-06 18:23:05 +0100

    change favicon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-purple-800 text-white text-end px-4 py-2'>
-      Hecho por Irene
-    </div>
+
+
+    <footer className="bg-purple-800 py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <div className="flex flex-wrap">
+          <a
+            href="/aviso-legal"
+            className="text-gray-300 hover:text-white mr-4"
+          >
+            Aviso legal
+          </a>
+          <a
+            href="/politica-de-cookies"
+            className="text-gray-300 hover:text-white mr-4"
+          >
+            Política de cookies
+          </a>
+          <a
+            href="/politica-de-privacidad"
+            className="text-gray-300 hover:text-white mr-4"
+          >
+            Política de privacidad
+          </a>
+        </div>
+
+        <a href="https://miwebpersonal.com" className="text-gray-300 hover:text-white">
+          Hecho por Irene
+        </a>
+      </div>
+    </footer>
+
+
+
   )
 }
 
