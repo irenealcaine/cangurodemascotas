86fa7c0a3f43b0b1b6a2012ab1db75bd530e75ff
ialcaine <irenealcainealvarez@gmail.com>
2023-04-27 09:11:53 +0000

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,27 +8,33 @@ const NavBar = () => {
     <nav className="bg-purple-800 py-2 sticky top-0">
       <div className="container mx-auto flex justify-between items-center px-4">
         <div className="flex justify-end w-full">
-          <Link
-            to="/legal"
+          <a
+            href="https://www.instagram.com/canguro.mascotas/"
+            target="_blank"
+            rel="noreferrer"
             className="text-gray-300 hover:text-white ml-4 flex items-center"
           >
             <FiInstagram />
             <p className="hidden md:block md:mx-2">canguro.mascotas</p>
-          </Link>
-          <Link
-            to="/cookies"
+          </a>
+          <a
+            href="https://api.whatsapp.com/send?phone=34664051872&text=Hola!"
+            target="_blank"
+            rel="noreferrer"
             className="text-gray-300 hover:text-white ml-4 flex items-center"
           >
             <BsWhatsapp />
             <p className="hidden md:block md:mx-2">664 05 18 72</p>
-          </Link>
-          <Link
-            to="/privacy"
+          </a>
+          <a
+            href="mailto:paulablasco3@gmail.com"
+            target="_blank"
+            rel="noreferrer"
             className="text-gray-300 hover:text-white ml-4 flex items-center"
           >
             <FiMail />
             <p className="hidden md:block md:mx-2">paulablasco3@gmail.com</p>
-          </Link>
+          </a>
         </div>
       </div>
     </nav>
