72fe1dcde6447e0069ab21989dba111aa1a8f818
ialcaine <irenealcainealvarez@gmail.com>
2023-04-28 06:38:47 +0000

    Update 2 files

--- src/components/Opinions.jsx
+++ src/components/Opinions.jsx
@@ -1,77 +1,30 @@
 import React from "react";
 import logo from "../img/Logo-removebg-preview.png";
+import opinions from "../components/db/opinionsdb.js";
 
 const Opinions = () => {
   return (
     <div className="p-4">
       <h2>Las opiniones de nuestros clientes</h2>
       <div className="flex flex-wrap flex-col md:flex-row justify-center items-center gap-4">
-        <div className="bg-white md:w-5/12 border-2 border-purple-800 rounded-xl relative p-4">
-          <img
-            alt="/"
-            src={logo}
-            className="absolute top-1 left-1 w-10 opacity-30"
-          />
-          <img
-            alt="/"
-            src={logo}
-            className="absolute bottom-1 right-1 w-10 opacity-30"
-          />
-          <p className="text-center text-xl">
-            Gracias Paula por cuidar de Travis tan bien. Volveremos. ❤️🐶
-          </p>
-          <p className="text-end italic">-- garridocanolaura</p>
-        </div>
-        <div className="bg-white md:w-5/12 border-2 border-purple-800 rounded-xl relative p-4">
-          <img
-            alt="/"
-            src={logo}
-            className="absolute top-0 left-0 w-10 opacity-30"
-          />
-          <img
-            alt="/"
-            src={logo}
-            className="absolute bottom-0 right-0 w-10 opacity-30"
-          />
-          <p className="text-center text-xl">
-            Max se lo pasó genial cuando os lo deje las pasadas vacaciones.
-            Gracias
-          </p>
-          <p className="text-end italic">-- davidpascualsoriano</p>
-        </div>
-        <div className="bg-white md:w-5/12 border-2 border-purple-800 rounded-xl relative p-4">
-          <img
-            alt="/"
-            src={logo}
-            className="absolute top-0 left-0 w-10 opacity-30"
-          />
-          <img
-            alt="/"
-            src={logo}
-            className="absolute bottom-0 right-0 w-10 opacity-30"
-          />
-          <p className="text-center text-xl">
-            ¡Rayo y Mía están felices! Muchas gracias ❤️ contigo los dejo
-            tranquila 😊
-          </p>
-          <p className="text-end italic">-- carmen.c.c.r</p>
-        </div>
-        <div className="bg-white md:w-5/12 border-2 border-purple-800 rounded-xl relative p-4">
-          <img
-            alt="/"
-            src={logo}
-            className="absolute top-0 left-0 w-10 opacity-30"
-          />
-          <img
-            alt="/"
-            src={logo}
-            className="absolute bottom-0 right-0 w-10 opacity-30"
-          />
-          <p className="text-center text-xl">
-            Que bien lo pasan y que felices están 😍👏
-          </p>
-          <p className="text-end italic">-- annablascosoria</p>
-        </div>
+        {opinions.map((opinion) => {
+          return (
+            <div className="bg-white md:w-5/12 border-2 border-purple-800 rounded-xl relative p-4">
+              <img
+                alt="/"
+                src={logo}
+                className="absolute top-1 left-1 w-10 opacity-30"
+              />
+              <img
+                alt="/"
+                src={logo}
+                className="absolute bottom-1 right-1 w-10 opacity-30"
+              />
+              <p className="text-center text-xl">{opinion.text}</p>
+              <p className="text-end italic">-- {opinion.author}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
