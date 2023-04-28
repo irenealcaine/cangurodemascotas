bf2d0a616b5dabaea00aae36c1f97ca838abea25
ialcaine <irenealcainealvarez@gmail.com>
2023-04-28 07:41:15 +0000

    Update NavBar.jsx and socialdb.js

--- src/components/db/socialdb.js
+++ src/components/db/socialdb.js
@@ -6,16 +6,19 @@ const socialMedia = [
     title: "canguro.mascotas",
     href: "https://www.instagram.com/canguro.mascotas/",
     icon: <FiInstagram className="text-purple-800" />,
+    navbarIcon: <FiInstagram />,
   },
   {
     title: "664 05 18 72",
     href: "https://api.whatsapp.com/send?phone=34664051872&text=Hola!",
     icon: <BsWhatsapp className="text-purple-800" />,
+    navbarIcon: <BsWhatsapp />,
   },
   {
     title: "paulablasco3@gmail.com",
     href: "mailto:paulablasco3@gmail.com",
     icon: <FiMail className="text-purple-800" />,
+    navbarIcon: <FiMail />,
   },
 ];
 export default socialMedia;
