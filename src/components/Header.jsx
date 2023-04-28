9a02ee98f507470c99f4940a9e0c3546e199338e
ialcaine <irenealcainealvarez@gmail.com>
2023-04-28 06:50:51 +0000

    Update Contact.jsx and Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-// import placeholder from '../img/placeholder header.png'
 import headerphoto from "../img/cabecera.png";
 import biglogo from "../img/Logo_Canguro_de_mascotas_Redes2-removebg-preview.png";
-// import { FiInstagram, FiMail } from "react-icons/fi";
-// import { BsWhatsapp } from "react-icons/bs";
 import socialMedia from "../components/db/socialdb.js";
 
 const Header = () => {
@@ -29,37 +26,6 @@ const Header = () => {
             </a>
           );
         })}
-
-        {/* <a
-          href="https://www.instagram.com/canguro.mascotas/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="flex gap-1 justify-center items-center  cursor-pointer hover:underline hover:text-purple-800  transition-all duration-500 ease-in-out text-lg md:text-2xl">
-            <FiInstagram className="text-purple-800" />
-            <p className="font-bold">canguro.mascotas</p>
-          </div>
-        </a>
-        <a
-          href="https://api.whatsapp.com/send?phone=34664051872&text=Hola!"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="flex gap-1 justify-center items-center hover:underline hover:text-purple-800 cursor-pointer transition-all duration-500 text-lg md:text-2xl">
-            <BsWhatsapp className="text-purple-800" />
-            <p className="font-bold">664 05 18 72</p>
-          </div>
-        </a>
-        <a
-          href="mailto:paulablasco3@gmail.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="flex gap-1 justify-center items-center hover:underline hover:text-purple-800 cursor-pointer transition-all duration-500 text-lg md:text-2xl">
-            <FiMail className="text-purple-800" />
-            <p className="font-bold">paulablasco3@gmail.com</p>
-          </div>
-        </a> */}
       </div>
     </div>
   );
