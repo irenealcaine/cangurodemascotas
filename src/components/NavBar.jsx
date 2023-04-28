675bffbae7ceab84a6787548f8165a7a67b6d6ea
ialcaine <irenealcainealvarez@gmail.com>
2023-04-28 07:48:58 +0000

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import socialMedia from "../components/db/socialdb.js";
+import logo from "../img/Logo-removebg-preview.png";
 
 const NavBar = () => {
   return (
     <nav className="bg-purple-800 py-2 sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center px-4">
+        <img
+          src={logo}
+          className="w-7 md:w-12 bg-white/80 rounded-full p-1 md:p-2"
+          alt="logo"
+        />
         <div className="flex justify-end w-full">
           {socialMedia.map((media) => {
             return (
